e9f430bc145da9569330e4fb01c164b4e8937f54
hugo <devhugok@gmail.com>
2023-11-14 17:06:27 +0100

    update

---
+++ functions.php
@@ -0,0 +1,23 @@
+<?php
+function nav_item ( string $lien, string $titre, string $linkClasss = '' ): string
+{
+    $classe = 'nav-item';
+    if ($_SERVER["SCRIPT_NAME"] && $_SERVER["SCRIPT_NAME"] == $lien){
+        $classe .= ' active';
+    }
+    return <<<HTML
+    <li class="$classe">
+        <a class="$classe" aria-current="page" href="$lien">$titre</a>
+    </li>
+    HTML;
+}
+
+function nav_menu (string $linkClass): string
+{
+    return 
+    nav_item ( '/index.php', 'Accueil' , $linkClass).
+    nav_item ( '/contact.php', 'Contact' , $linkClass);
+}
+    
+    
+?>
